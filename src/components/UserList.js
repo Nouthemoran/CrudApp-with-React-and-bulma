300db99273dd0a2b3ec8a024ddccb1bbd177dcbb
Nouthemoran <mardiansahafiz@gmail.com>
2024-01-29 17:47:13 +0700

    menambahkan fitur update dan delete

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 const UserList = () => {
 const [users, setUser] = useState([]);
@@ -11,10 +12,21 @@ useEffect(()=>{
 const getUsers = async () => {
     const response = await axios.get('http://localhost:5000/users');
     setUser(response.data)
-}
+};
+
+const deleteUser = async (id) => {
+    try {
+        await axios.delete(`http://localhost:5000/users/${id}`);
+        getUsers();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
   return (
     <div className="columns mt-5 is-centered">
         <div className="columns is-half">
+            <Link to={'add'} className="button is-success">Add New</Link>
            <table className='table is-striped is-fullwidth'>
             <thead>
                 <tr>
@@ -33,8 +45,8 @@ const getUsers = async () => {
                         <td>{user.email}</td>
                         <td>{user.gender}</td>
                         <td>
-                            <button className="button is-small is-info">Edit</button>
-                            <button className="button is-small is-danger">Delete</button>
+                            <Link to={`edit/${user.id}`} className="button is-small is-info">Edit</Link>
+                            <button onClick={() => deleteUser(user.id)} className="button is-small is-danger">Delete</button>
                         </td>
                     </tr>
                 ))}

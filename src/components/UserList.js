54324dc298ed97a43b9757abc0f690fcc30c94f5
Nouthemoran <mardiansahafiz@gmail.com>
2024-01-29 11:01:27 +0700

    membuat fitur create dan read

---
+++ src/components/UserList.js
@@ -0,0 +1,48 @@
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+
+const UserList = () => {
+const [users, setUser] = useState([]);
+
+useEffect(()=>{
+    getUsers();
+},[]);
+
+const getUsers = async () => {
+    const response = await axios.get('http://localhost:5000/users');
+    setUser(response.data)
+}
+  return (
+    <div className="columns mt-5 is-centered">
+        <div className="columns is-half">
+           <table className='table is-striped is-fullwidth'>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user, index) => (
+                    <tr key={user.id}>
+                        <td>{index + 1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.gender}</td>
+                        <td>
+                            <button className="button is-small is-info">Edit</button>
+                            <button className="button is-small is-danger">Delete</button>
+                        </td>
+                    </tr>
+                ))}
+
+            </tbody>
+           </table>       </div>
+    </div>
+  )
+}
+
+export default UserList
